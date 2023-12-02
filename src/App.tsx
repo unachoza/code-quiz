61171934c09243d12873f6f78461694e052f9e81
arianna choza <unachoza@gmail.com>
2023-12-01 22:29:29 -0500

    select quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+    //@ts-nocheck
+import React, { MouseEventHandler } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button/Button'
 import Nav from './components/Nav/Nav'
 import Options from './components/Form/Options/Options';
+import data from "./data/data.json"
+import { clickOptions } from '@testing-library/user-event/dist/click';
+
+const quizNames = [ "HTML", "CSS", "JavaScript","Accessibility" ]
 
 function App() {
+
+  let selectedQuiz:string = ""
+
+  const selectQuiz = (event: MouseEventHandler<HTMLButtonElement>) => {
+    selectedQuiz = (data?.quizzes.filter((q:any) => q.title == event.target.name)).shift()
+    return selectedQuiz
+  }
+
   return (
     <div className="App">
       <Nav/>
       <div className='question-container'>  
-
       </div>
       <div className='Options-container'>
-      <Options/>
+        <h1>Pick a Subject to get Started</h1>
+        {quizNames.map((name:string) => <Button text={name} onClick={(e) => selectQuiz(e)} />)}
+      <Options quiz={selectedQuiz}/>
       </div>
      <Button text="Submit Answer" onClick={(e)=> console.log("clicked", e)}/>
     </div>
